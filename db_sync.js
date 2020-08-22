96c72df143d864c2ebba0cbe917b3a738cd2bd13
X3ZvaWQ <x3zvawq@gmail.com>
2020-08-22 17:02:24 +0800

    fix: db sync not Sequelize instance passed

--- db_sync.js
+++ db_sync.js
@@ -1,5 +1,14 @@
+const ENV = require('./env');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(ENV.db_database, ENV.db_username, ENV.db_password, {
+  dialect: ENV.db_dialect,
+  host: ENV.db_host,
+  port: ENV.db_port
+});
+global.sequelize = sequelize;
+
 const Alias = require("./src/model/alias");
-const { flowerPrice } = require("./src/route");
+const flowerPrice = require("./src/model/flowerPrice");
 
 Alias.sync();
 flowerPrice.sync();
