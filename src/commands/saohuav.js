1ac634b1ff24b93ae404dd4b13898a28e570be0d
X3ZvaWQ <x3zvawq@gmail.com>
2021-06-26 19:21:10 +0800

    fix: saohuav api

--- src/commands/saohuav.js
+++ src/commands/saohuav.js
@@ -1,8 +1,8 @@
-const Api = require('../service/api');
+const Jx3api = require('../service/httpApi/jx3api');
 
 module.exports = class SaoHuaHandler {
     async handle() {
-        let saohuav = await Api.getSaoHuaFromJx3Api();
+        let saohuav = await Jx3api.saohua();
         return `[CQ:tts,text=${saohuav}]`;
     }
 
--- src/service/api.js
+++ src/service/api.js
@@ -3,22 +3,6 @@ const md5 = require('js-md5');
 const ENV = require('../../env.json');
 
 class Api{ 
-  static async getSaoHuaFromJx3Api() {
-        let url = `${ENV.jx3api_baseurl}app/random`;
-        let response = await axios.get(url,{
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-                'Accept': 'application/json, text/plain, */*'
-            }
-        });
-        if(response.data.code == 200) {
-            let result = response.data.data.text;
-            return result;
-        }else{
-            throw '你看，你也缺情缘，我也缺情缘，你密我，我们就都不缺情缘了';
-        }
-    }
-
     static async getDouTuFromJx3Api() {
         let url = `https://jx3api.com/extend/expression`;
         let response = await axios.get(url,{
