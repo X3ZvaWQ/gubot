220ef7668e656b6727dfa3eb4726f0f7a7e6b778
X3ZvaWQ <x3zvawq@gmail.com>
2021-07-09 23:09:34 +0800

    fix: 401 catch

--- src/service/httpApi/jx3tuilan.js
+++ src/service/httpApi/jx3tuilan.js
@@ -8,16 +8,20 @@ class Jx3tuilan{
     }
 
     static async jjc_info(name, server) {
-        let response = $tuilan.get('/get_role_jjc_info_by_name', {
-            params: {
-                name: name,
-                server: server
+        try {
+            let response = $tuilan.get('/get_role_jjc_info_by_name', {
+                params: {
+                    name: name,
+                    server: server
+                }
+            });
+            if(!response.data || response.data.msg != '查询成功！'){
+                throw `错误：[${Jx3tuilan.apiDisplayName}]的接口[jjc_info]返回值异常，请检查参数。`;
             }
-        });
-        if(!response.data || response.data.msg != '查询成功！'){
-            throw `错误：[${Jx3tuilan.apiDisplayName}]的接口[jjc_info]返回值异常，请检查参数。`;
+            return response.data;
+        } catch (error) {
+            throw error.response.data.msg;
         }
-        return response.data;
     }
 }
 
