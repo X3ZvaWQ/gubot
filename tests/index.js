0612b548a00487c09db5953614ef6c851eaa79da
X3ZvaWQ <x3zvawq@gmail.com>
2021-07-09 23:02:51 +0800

    add: jjc info

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,18 @@
-const ENV = require('../env.json');
+const art = require('art-template');
+const fs = require('fs-extra');
+
+fs.readFile('/d/workspace/gubot/src/templates/jjc.html').then((source) => {
+    fs.readFile('/d/workspace/gubot/tests/data.json').then((data) => {
+        data = JSON.parse(data.toString());
+        source = source.toString();
+
+        console.log(data);
+        let a = art.render(source, {data: data});
+        fs.outputFile('./aaaa.html', a);
+    });
+});
+
+/* const ENV = require('../env.json');
 const tencentcloud = require("tencentcloud-sdk-nodejs-nlp")
 const NlpClient = tencentcloud.nlp.v20190408.Client;
 const uuid = require('uuid').v4;
@@ -29,7 +43,7 @@ client.TextToVoice = function (text) {
         console.log(x);
     })
 }
-client.TextToVoice("咕咕咕，鸽子飞走啦");
+client.TextToVoice("咕咕咕，鸽子飞走啦"); */
 
 /* require('colors');
 (async () => {
