5267a2466dd59eb123b999fd566501b08f433032
X3ZvaWQ <x3zvawq@gmail.com>
2023-08-06 13:33:00 +0800

    fix: bugs

--- src/database/db_sync.js
+++ src/database/db_sync.js
@@ -1,22 +1,23 @@
-const ENV = require('../../env.json').database;
-const Sequelize = require('sequelize');
+const ENV = require("../../env.json").database;
+const Sequelize = require("sequelize");
 const sequelize = new Sequelize(ENV.database, ENV.username, ENV.password, {
-  dialect: ENV.dialect,
-  host: ENV.host,
-  port: ENV.port
+    dialect: ENV.dialect,
+    host: ENV.host,
+    port: ENV.port,
 });
 global.bot = {};
 bot.sequelize = sequelize;
 
 const models = {
-  alias : require("../model/alias"),
-  user : require('../model/user'),
-  group : require('../model/group'),
-  team: require('../model/team'),
-  groupOption: require('../model/botOption'),
-  botOption: require('../model/team'),
-  bot: require('../model/bot'),
-  team: require('../model/team')
-}
+    alias: require("../model/alias"),
+    user: require("../model/user"),
+    group: require("../model/group"),
+    team: require("../model/team"),
+    groupOption: require("../model/botOption"),
+    botOption: require("../model/team"),
+    bot: require("../model/bot"),
+    team: require("../model/team"),
+    subscribe: require("../model/subscribe"),
+};
 
 sequelize.sync({ alter: true });
